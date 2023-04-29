cc5bbe5f19522a2953fcc968110e3254b078b4c8
sohel <mr.sohelsyed@gmail.com>
2023-04-29 18:23:32 +0530

    added basic project structure

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -3,7 +3,7 @@ import { Box, Heading, Text } from "@chakra-ui/react";
 const HomePage = () => {
   return (
     <Box
-      backgroundImage="url('/src/assets/hero-img.jpg')"
+      backgroundImage="url('https://img.freepik.com/free-photo/young-handsome-physician-medical-robe-with-stethoscope_1303-17818.jpg?w=996&t=st=1682772779~exp=1682773379~hmac=9394dd2d96798395cb59dbf4eddcc33cd7d7eeaddc8a0ca5c5f1d069e0e88ec4')"
       backgroundPosition="center"
       backgroundRepeat="no-repeat"
       bgSize="100%"
