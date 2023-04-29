19271362e2ebba041adb56b1733f573cc1eb52f4
sohel <mr.sohelsyed@gmail.com>
2023-04-29 19:04:14 +0530

    working on footer

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,8 +1,42 @@
+import { Box, Text } from "@chakra-ui/react";
 
 const Footer = () => {
   return (
-    <div>Footer</div>
-  )
-}
+    <Box
+      backgroundColor={"#61876e"}
+      color={"#fafaf1"}
+      w={"100%"}
+      h={"35vh"}
+      display={"flex"}
+      p={"50px"}
+    >
+      <Box display={"flex"} w={"80%"} m={"auto"}>
+        <Box w={"220px"}>Logo</Box>
+        <Box w={"220px"}>
+          <Text as={"b"}>Menu</Text>
+          <Text>Home</Text>
+          <Text>About us</Text>
+          <Text>Servies</Text>
+          <Text>Appointment</Text>
+          <Text>Language</Text>
+        </Box>
+        <Box w={"220px"}>
+          <Text as={"b"}>Need Help ?</Text>
+          <Text>Medical Q&As</Text>
+          <Text>Terms of use</Text>
+          <Text>Privacy Policy</Text>
+          <Text>Doctors Privacy Policy</Text>
+        </Box>
+        <Box w={"220px"}>
+          <Text as={"b"}>Contacts</Text>
+          <Text>+2 000 000 000</Text>
+          <Text>14 Grean Road ST.</Text>
+          <Text>kmlhospital155@gmail.com</Text>
+        </Box>
+        <Box w={"220px"}>Follow us On</Box>
+      </Box>
+    </Box>
+  );
+};
 
-export default Footer
+export default Footer;
