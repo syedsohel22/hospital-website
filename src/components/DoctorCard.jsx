eed080dc3fb852810fdbfbf6b92e1ce183c5648d
sohel <mr.sohelsyed@gmail.com>
2023-04-30 23:46:25 +0530

    working on doctor card

--- src/components/DoctorCard.jsx
+++ src/components/DoctorCard.jsx
@@ -1,5 +1,14 @@
+import { Box, Heading, Image, Text } from "@chakra-ui/react";
 const DoctorCard = () => {
-  return <div>DoctorCard</div>;
+  return (
+    <Box>
+      <Heading></Heading>
+      <Image src="" alt="" />
+      <Text></Text>
+      <Text></Text>
+      <Text></Text>
+    </Box>
+  );
 };
 
 export default DoctorCard;
