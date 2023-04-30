9eb85c1ee5b031c427c7e80e87a93be74cd6f686
sohel <mr.sohelsyed@gmail.com>
2023-04-30 23:44:26 +0530

    working on login

--- README.md
+++ README.md
@@ -18,3 +18,7 @@
 - one : #fafaf1 (lite Background color)
 - two : #61876e (footer main color)
 - three : #365349 (dark green Color)
+
+## members
+
+- sohel -payment, checkout
--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,5 +1,45 @@
+import {
+  Box,
+  FormControl,
+  FormLabel,
+  HStack,
+  Heading,
+  Input,
+  Stack,
+} from "@chakra-ui/react";
+
 const Login = () => {
-  return <div>Login</div>;
+  return (
+    <Box display={"flex"} justifyContent={"center"}>
+      <Heading>CREATE ACCOUNT</Heading>
+      <Box
+        display={"flex"}
+        w={"40%"}
+        m={"auto"}
+        border={"1px solid black"}
+        justifyContent={"center"}
+      >
+        <Box>
+          <Stack>
+            <HStack>
+              <Box>
+                <FormControl id="firstName" isRequired>
+                  <FormLabel>First Name</FormLabel>
+                  <Input type="text" />
+                </FormControl>
+              </Box>
+              <Box>
+                <FormControl id="lastName">
+                  <FormLabel>Last Name</FormLabel>
+                  <Input type="text" />
+                </FormControl>
+              </Box>
+            </HStack>
+          </Stack>
+        </Box>
+      </Box>
+    </Box>
+  );
 };
 
 export default Login;
